e634b1351f7dd1c7e089aee232a788e7e288b8f9
Janis-byte <141801777+Janis-byte@users.noreply.github.com>
2024-08-27 10:06:44 +0700

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,73 +1,30 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_simple_calculator/flutter_simple_calculator.dart';
+import 'package:flutter_application_4/ListView.dart';
+import 'package:flutter_application_4/Stack.dart';
+import 'package:flutter_application_4/dateandtimepicker.dart';
+import 'package:flutter_application_4/dialog.dart';
+import 'package:flutter_application_4/image_widget.dart';
+import 'package:flutter_application_4/scaffold.dart';
+import 'package:flutter_application_4/tesScaffold.dart';
+import 'package:flutter_application_4/text_widget.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('SimpleCalculator'),
-        ),
-        body: const Padding(
-          padding: EdgeInsets.all(18.0),
-          child: SizedBox(
-            width: double.infinity,
-            child: CalcButton(),
-          ),
-        ),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-    );
-  }
-}
-
-class CalcButton extends StatefulWidget {
-  const CalcButton({Key? key}) : super(key: key);
-
-  @override
-  CalcButtonState createState() => CalcButtonState();
-}
-
-class CalcButtonState extends State<CalcButton> {
-  double? _currentValue = 0;
-  @override
-  Widget build(BuildContext context) {
-    var calc = SimpleCalculator(
-      value: _currentValue!,
-      hideExpression: false,
-      hideSurroundingBorder: true,
-      autofocus: true,
-      onChanged: (key, value, expression) {
-        setState(() {
-          _currentValue = value ?? 0;
-        });
-        if (kDebugMode) {
-          print('$key\t$value\t$expression');
-        }
-      },
-      onTappedDisplay: (value, details) {
-        if (kDebugMode) {
-          print('$value\t${details.globalPosition}');
-        }
-      },
-    );
-    return OutlinedButton(
-      child: Text(_currentValue.toString()),
-      onPressed: () {
-        showModalBottomSheet(
-            isScrollControlled: true,
-            context: context,
-            builder: (BuildContext context) {
-              return SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.75,
-                  child: calc);
-            });
-      },
+      home: Tesscaffold(),
     );
   }
 }
